b530b1334a42795fc352468f1d856979429d148a
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2022-05-26 15:11:19 -0700

    Add new controller

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,16 @@
+using API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+    }
+}
