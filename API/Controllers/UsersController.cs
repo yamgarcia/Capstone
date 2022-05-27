2b27802cc0c8045fa756f04a4ca1421cb54e9761
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2022-05-26 22:23:37 -0700

    Fix git files to be tracked

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -15,15 +16,15 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
-            return _context.Users.ToList();
+            return await _context.Users.ToListAsync();
         }
 
         // api/user/3 = Jean's ID
         [HttpGet("{id}")]
-        public ActionResult<AppUser> GetUsers(int id) {
-            return _context.Users.Find(id);
+        public async Task<ActionResult<AppUser>> GetUsers(int id) {
+            return await _context.Users.FindAsync(id);
         } 
         
     }
