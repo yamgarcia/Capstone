5e8c3b9e554c902efa34e562c81d85e847e68537
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2022-05-30 13:23:38 -0700

    Add Validation to Register Controller
    Add RegisterDTO to send objects to the request body

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,8 +1,10 @@
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -16,13 +18,16 @@ namespace API.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(string username, string password){
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto){
+
+            if (await UserExists(registerDto.Username)) return BadRequest("Username Taken");
+
             // "using" is used to sign an object that must be disposed after it's purpose is fulfilled.
             // It calls a method inside the instantiated class that properly dispose it. 
            using var hmac = new HMACSHA512(); 
            var user = new AppUser{
-               UserName = username,
-               PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+               UserName = registerDto.Username.ToLower(),
+               PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                PasswordSalt = hmac.Key
            };
             // "Add " method doesn't add to the DB but to he tracking in place
@@ -31,6 +36,10 @@ namespace API.Controllers
            await _context.SaveChangesAsync();
 
            return user;
+        }
+
+        private async Task<bool> UserExists (string username){
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
     }
 }
