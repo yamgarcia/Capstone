6457b46d08426d3199c5a2318c282e03b2a458d9
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2022-05-30 14:07:58 -0700

    Add DTO for Login
    Creat a Login action with hashble pw

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -38,6 +38,23 @@ namespace API.Controllers
            return user;
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto){
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+
+            if (user == null) return Unauthorized("Invilid Username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++){
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
+            }
+
+            return user;
+        }
+
         private async Task<bool> UserExists (string username){
             return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
