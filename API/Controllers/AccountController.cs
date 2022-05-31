1ee4743313b512ae1525c768d1cc7b8077e6eff1
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2022-05-30 20:44:05 -0700

    Add Token Key
    Refactor with UserDTO

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,35 +12,43 @@ namespace API.Controllers
     public class AccountController : BaseApiController
     {
         private readonly DataContext _context;
+        private readonly ITokenService _tokenService;
 
-        public AccountController(DataContext context)
+        public AccountController(DataContext context, ITokenService tokenService)
         {
+            _tokenService = tokenService;
             _context = context;
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto){
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
 
             if (await UserExists(registerDto.Username)) return BadRequest("Username Taken");
 
             // "using" is used to sign an object that must be disposed after it's purpose is fulfilled.
             // It calls a method inside the instantiated class that properly dispose it. 
-           using var hmac = new HMACSHA512(); 
-           var user = new AppUser{
-               UserName = registerDto.Username.ToLower(),
-               PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-               PasswordSalt = hmac.Key
-           };
+            using var hmac = new HMACSHA512();
+            var user = new AppUser
+            {
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
             // "Add " method doesn't add to the DB but to he tracking in place
             _context.Users.Add(user);
 
-           await _context.SaveChangesAsync();
+            await _context.SaveChangesAsync();
 
-           return user;
+            return new UserDto{
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
         }
 
         [HttpPost("login")]
-        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto){
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
             var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
 
             if (user == null) return Unauthorized("Invilid Username");
@@ -48,14 +57,19 @@ namespace API.Controllers
 
             var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
 
-            for (int i = 0; i < computedHash.Length; i++){
+            for (int i = 0; i < computedHash.Length; i++)
+            {
                 if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
             }
 
-            return user;
+            return new UserDto{
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
         }
 
-        private async Task<bool> UserExists (string username){
+        private async Task<bool> UserExists(string username)
+        {
             return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
     }
