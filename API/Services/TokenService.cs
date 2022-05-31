2fb27348d436f5c6337ce39de28ec97e314c696a
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2022-05-30 17:34:39 -0700

    Create and setup Token generating class

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -1,13 +1,43 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using API.Entities;
 using API.Interfaces;
+using Microsoft.IdentityModel.Tokens;
 
 namespace API.Services
 {
     public class TokenService : ITokenService
     {
+        private readonly SymmetricSecurityKey _key;
+        public TokenService(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        }
+
         public string CreateToken(AppUser user)
         {
-            throw new NotImplementedException();
+            //Adding claims
+            var claims = new List<Claim> 
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+            };
+            //Creating Credentials 
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+            //Describe how the token should look
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds
+                
+            };          
+            //Innitialize Handler for token creation
+            var tokenHandler = new JwtSecurityTokenHandler();
+            //Create Token
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            //Return it Written
+            return tokenHandler.WriteToken(token);      
         }
     }
 }
