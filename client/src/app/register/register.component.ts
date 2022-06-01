3628046b46c65f7b3fa830e454283e353e95d963
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2022-06-01 16:40:53 -0700

    Implement register user feature

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,21 +1,29 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AccountService } from './../_services/account.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
-
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor() {}
+  constructor(private accountService: AccountService) {}
 
   ngOnInit(): void {}
 
   register() {
-    console.log(this.model);
+    this.accountService.register(this.model).subscribe(
+      (res) => {
+        console.log(res);
+        this.cancel();
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
 
   cancel() {
