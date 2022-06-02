3fe9a22a23b3f83ec782bdb7f928300c9bbbde03
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2022-06-02 14:23:17 -0700

    Setup guard and protect links with authentication through an observable

--- client/src/app/_guards/auth.guard.ts
+++ client/src/app/_guards/auth.guard.ts
@@ -1,24 +1,27 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  RouterStateSnapshot,
-  UrlTree,
-} from '@angular/router';
+import { CanActivate } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AccountService } from './../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
+import { map } from 'rxjs/operators';
+import { User } from './../_models/user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ):
-    | Observable<boolean | UrlTree>
-    | Promise<boolean | UrlTree>
-    | boolean
-    | UrlTree {
-    return true;
+
+  constructor(
+    private accountService: AccountService,
+    private toastr: ToastrService
+  ) {}
+
+  canActivate(): Observable<boolean> {
+    return this.accountService.currentUser$.pipe(
+      map((user: User) => {
+        if (user) return true;
+        this.toastr.error("You shall not pass!!")
+      })
+    );
   }
 }
