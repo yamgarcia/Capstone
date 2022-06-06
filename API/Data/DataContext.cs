a51dfb37b86040ab2a2dcc599dc4510aebd07f6c
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2022-06-06 11:58:25 -0700

    Add comments

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -3,12 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
+    //* DbContext corresponds to a Database
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions options) : base(options)
         {
         }
-
+        //* DbSet corresponds to each table in context 
         public DbSet<AppUser> Users { get; set; }
     }
 }
