f0c6fa0c4d9adef0d71ef8a13123c5603b96d180
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2022-06-05 21:19:07 -0700

    Add not props to AppUser
    Add extension to find age

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,3 +1,5 @@
+using API.Extensions;
+
 namespace API.Entities
 {
     public class AppUser
@@ -24,12 +26,17 @@ namespace API.Entities
 
         public string LookingFor { get; set; }
 
-        public string Interests { get; set; }
+        public string Skills { get; set; }
 
         public string City { get; set; }
 
         public string Country { get; set; }
 
         public ICollection<Photo> Photos { get; set; }
+
+        public int GetAge()
+        {
+            return DateOfBirth.CalculateAge();
+        }
     }
 }
