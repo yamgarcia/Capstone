03e6aa4e6f9f8f4b1fc9ddd337be9fdb36b1424a
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2022-06-09 23:20:57 -0700

    Set up file uploader - ng2-file-upload

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,18 +1,58 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
+import { User } from 'src/app/_models/user';
 import { Member } from 'src/app/_modules/member';
+import { environment } from './../../../environments/environment';
+import { AccountService } from './../../_services/account.service';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-photo-editor',
   templateUrl: './photo-editor.component.html',
-  styleUrls: ['./photo-editor.component.css']
+  styleUrls: ['./photo-editor.component.css'],
 })
 export class PhotoEditorComponent implements OnInit {
   // Receive member from parent component
-  @Input() member: Member
+  @Input() member: Member;
+  uploader: FileUploader;
+  hasBaseDropzoneOver = false;
+  baseUrl: string = environment.apiUrl;
+  user: User;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private accountService: AccountService) {
+    this.accountService.currentUser$
+      .pipe(take(1))
+      .subscribe((user) => (this.user = user));
   }
 
+  ngOnInit(): void {
+    this.initializedUploader();
+  }
+
+  fileOverBase(e: any) {
+    this.hasBaseDropzoneOver = e;
+  }
+
+  initializedUploader() {
+    this.uploader = new FileUploader({
+      url: this.baseUrl + 'users/add-photo',
+      authToken: 'Bearer ' + this.user.token,
+      isHTML5: true,
+      allowedFileType: ['image'],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024,
+    });
+
+    this.uploader.onAfterAddingFile = (file) => {
+      file.withCredentials = false;
+    };
+
+    this.uploader.onSuccessItem = (item, response, status, headers) => {
+      if (response) {
+        const photo = JSON.parse(response);
+        this.member.photos.push(photo);
+      }
+    };
+  }
 }
