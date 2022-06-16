2ab57df89a6bd5550cf9ec7fd14ef79a2d3a724f
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2022-06-16 14:00:05 -0700

    Update Interface Documentation

--- API/Interfaces/IMessageRepository.cs
+++ API/Interfaces/IMessageRepository.cs
@@ -6,16 +6,18 @@ namespace API.Interfaces
 {
     public interface IMessageRepository
     {
-         void AddMessage(Message message);
+        void AddMessage(Message message);
 
-         void DeleteMessage(Message message);
+        void DeleteMessage(Message message);
 
-         Task<Message> GetMessage(int id);
+        Task<Message> GetMessage(int id);
 
-         Task<PagedList<MessageDto>> GetMessagesForUser();
+        ///<summary> Returns a paged list of messages </summary>
+        Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams);
 
-         Task<IEnumerable<MessageDto>> GetMessageThread(int currentUserId, int recipientId);
+        ///<summary> Returns a Message Thread between two users </summary>
+        Task<IEnumerable<MessageDto>> GetMessageThread(string currentUsername, string recipientUsername);
 
-         Task<bool> SaveAllAsync();
+        Task<bool> SaveAllAsync();
     }
 }
