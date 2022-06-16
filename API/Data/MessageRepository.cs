24d805d078acb25706179f0beafc2707a0368cbf
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2022-06-16 14:05:00 -0700

    Update message repository

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -2,14 +2,22 @@ using API.DTOs;
 using API.Entities;
 using API.Helpers;
 using API.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
+    ///<summary>The Repository pattern. Repositories are classes or components that encapsulate the logic required to access data
+    /// sources. They centralize common data access functionality, providing better maintainability and decoupling th
+    ///e infrastructure or technology used to access databases from the domain model layer.</summary>
     public class MessageRepository : IMessageRepository
     {
         private readonly DataContext _context;
-        public MessageRepository(DataContext context)
+        private readonly IMapper _mapper;
+        public MessageRepository(DataContext context, IMapper mapper)
         {
+            _mapper = mapper;
             _context = context;
         }
 
@@ -28,14 +36,61 @@ namespace API.Data
             return await _context.Messages.FindAsync(id);
         }
 
-        public Task<PagedList<MessageDto>> GetMessagesForUser()
+        public async Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams)
         {
-            throw new NotImplementedException();
+            var query = _context.Messages
+               //order by most recent
+               .OrderByDescending(m => m.MessageSent)
+               .AsQueryable();
+
+            // Applies .where based on the "Container" return value
+            // Recipients who read the message receive it in the Inbox
+            query = messageParams.Container switch
+            {
+                "Inbox" => query.Where(u => u.Recipient.UserName == messageParams.Username
+                    && u.RecipientDeleted == false),
+                "Outbox" => query.Where(u => u.Sender.UserName == messageParams.Username
+                    && u.SenderDeleted == false),
+                _ => query.Where(u => u.Recipient.UserName ==
+                    messageParams.Username && u.RecipientDeleted == false && u.DateRead == null)
+            };
+
+            var messages = query.ProjectTo<MessageDto>(_mapper.ConfigurationProvider);
+
+            return await PagedList<MessageDto>.CreateAsync(messages, messageParams.PageNumber, messageParams.PageSize);
         }
 
-        public Task<IEnumerable<MessageDto>> GetMessageThread(int currentUserId, int recipientId)
+        public async Task<IEnumerable<MessageDto>> GetMessageThread(string currentUsername, string recipientUsername)
         {
-            throw new NotImplementedException();
+            var messages = await _context.Messages
+                .Include(u => u.Sender).ThenInclude(p => p.Photos)
+                .Include(u => u.Recipient).ThenInclude(p => p.Photos)
+                .Where(m => m.Recipient.UserName == currentUsername && m.RecipientDeleted == false
+                        && m.Sender.UserName == recipientUsername
+                        || m.Recipient.UserName == recipientUsername
+                        && m.Sender.UserName == currentUsername && m.SenderDeleted == false
+                )
+                .OrderBy(m => m.MessageSent)
+                .ToListAsync();
+            
+            //Populates a List<> with unread messages by the current user
+            var unreadMessages = messages.Where(m => m.DateRead == null
+                && m.Recipient.UserName == currentUsername).ToList();
+
+
+            //any messages meeting the previous conditious are marked as read and save to the DB
+            if (unreadMessages.Any())
+            {
+                foreach (var message in unreadMessages)
+                {
+                    message.DateRead = DateTime.Now;
+                }
+
+                await _context.SaveChangesAsync();
+            }
+
+            // return the messages of type MessageDto
+            return _mapper.Map<IEnumerable<MessageDto>>(messages);
         }
 
         public async Task<bool> SaveAllAsync()
