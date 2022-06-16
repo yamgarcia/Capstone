78f7a29749d38d89e814d8f66ccc66132be011a2
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2022-06-16 14:04:10 -0700

    Update MessagesControler with new get requests

--- API/Controllers/MessagesController.cs
+++ API/Controllers/MessagesController.cs
@@ -2,6 +2,7 @@
 using API.DTOs;
 using API.Entities;
 using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
@@ -15,7 +16,7 @@ namespace API.Controllers
         private readonly IUserRepository _userRepository;
         private readonly IMessageRepository _messageRepository;
         private readonly IMapper _mapper;
-        public MessagesController(IUserRepository userRepository, IMessageRepository messageRepository, 
+        public MessagesController(IUserRepository userRepository, IMessageRepository messageRepository,
             IMapper mapper)
         {
             _mapper = mapper;
@@ -52,5 +53,31 @@ namespace API.Controllers
             return BadRequest("Failed to send message");
 
         }
+
+        [HttpGet]
+        //[FromQuery] reminds it the this param are going to be populated by the query ({{url}}/api/messages?container=Outbox)
+        public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessagesForUser([FromQuery]
+            MessageParams messageParams)
+        {
+            messageParams.Username = User.GetUsername();
+
+            var messages = await _messageRepository.GetMessagesForUser(messageParams);
+
+            Response.AddPaginationHeader(messages.CurrentPage, messages.PageSize,
+                messages.TotalCount, messages.TotalPages);
+
+            return messages;
+        }
+
+        [HttpGet("thread/{username}")]
+        ///<summary> the root param thread must be followed by the other's {username} (not the current)</summary>
+        public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessageThread(string username)
+        {
+            var currentUsername = User.GetUsername();
+
+            return Ok(await _messageRepository.GetMessageThread(currentUsername, username));
+        }
+
     }
+
 }
