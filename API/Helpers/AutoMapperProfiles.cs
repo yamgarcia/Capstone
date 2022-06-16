6fe417765b847dab88ed428fca81b64c62ea6e4f
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2022-06-15 18:51:28 -0700

    Add AutoMapper configuration for the Message/MessageDto

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -17,6 +17,12 @@ namespace API.Helpers
             CreateMap<Photo, PhotoDto>();
             CreateMap<MemberUpdateDto, AppUser>();
             CreateMap<RegisterDto, AppUser>();
+            //destination = MmemberDto field to map
+            //Sender = AppUser in the Message entity
+            CreateMap<Message, MessageDto>().ForMember(destination => destination.SenderPhotoUrl, options => options.MapFrom(src =>
+                    src.Sender.Photos.FirstOrDefault(x => x.IsMain).Url));
+            CreateMap<Message, MessageDto>().ForMember(destination => destination.RecipientPhotoUrl, options => options.MapFrom(src =>
+                    src.Recipient.Photos.FirstOrDefault(x => x.IsMain).Url));
         }
     }
 }
