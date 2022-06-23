a0d522d20128ea4e6f2c3fc12e654e9c164f5208
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2022-06-23 15:02:19 -0700

    Import Presence to use in to front end

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Member } from 'src/app/_modules/member';
 import { MembersService } from 'src/app/_services/members.service';
 import { ToastrService } from 'ngx-toastr';
+import { PresenceService } from 'src/app/_services/presence.service';
 
 @Component({
   selector: 'app-member-card',
@@ -14,7 +15,8 @@ export class MemberCardComponent implements OnInit {
 
   constructor(
     private membersService: MembersService,
-    private toastr: ToastrService
+    private toastr: ToastrService,
+    public presence: PresenceService
   ) {}
 
   ngOnInit(): void {}
