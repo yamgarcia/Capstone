cb98318cec83399a061bd9473e31515dc9a09864
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2022-06-27 16:36:44 -0700

    Remove unnecessary statement | UTC no enforced in dataContext

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -24,7 +24,6 @@ namespace API.Helpers
                     src.Sender.Photos.FirstOrDefault(x => x.IsMain).Url))
                 .ForMember(destination => destination.RecipientPhotoUrl, options => options.MapFrom(src =>
                     src.Recipient.Photos.FirstOrDefault(x => x.IsMain).Url));
-            CreateMap<DateTime, DateTime>().ConvertUsing(d => DateTime.SpecifyKind(d, DateTimeKind.Utc));
         }
     }
 }
